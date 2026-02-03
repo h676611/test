4952f791b04e653f7d5ec5e582743a8902b351ff
LarsPLoge <larsloge9@gmail.com>
2026-02-03 15:46:45 +0100

    Update client.py

--- client.py
+++ client.py
@@ -6,17 +6,22 @@ import json
 from helper.requestKomponents import generateRequest
 
 class Client:
-     def __init__(self, address="tcp://localhost:5555"):
-         self.context = zmq.Context()
-         self.socket = self.context.socket(zmq.DEALER)
-         self.socket.connect(address)
+    def __init__(self, address="tcp://localhost:5555"):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.DEALER)
+        self.socket.connect(address)
 
-     def send_request(self, request):
-         self.socket.send_json(request)
-         reply = []
-         reply.append(self.socket.recv_json())
-         reply.append(self.socket.recv_json())
-         return reply
+    def send_request(self, request):
+        self.socket.send_json(request)
+        reply = []
+        i = 0
+        while i < len(request["payload"]):
+            i +=1
+            reply.append(self.socket.recv_json())
+        return reply
+
+        
+
 if __name__ == "__main__":
     client = Client()
     address = "ASRL1::INSTR"
